59c615b25d3e04c3bab66a38fac6fe840d3961c3
Alejandro Aguayo-Ortiz <aaguayoo92@gmail.com>
2022-06-21 15:40:18 -0500

    feat: Se agregó empleo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,8 @@ import numpy as np
 st.title("Encuesta egresados")
 st.header("Esto es un encabezado")
 
-years = list(np.linspace(2000,2022,1))
-st.checkbox("",years)
+with st.form("Años empleo"):
+    years = list(np.linspace(2000,2022,1))
+    col1, col2 = st.columns(2)
+    start = col1.selectbox("",years)
+    end = col2.selectbox("",years)
